84d74bf95844a94097c7bff1a8d129a871190ca8
Johnny Choo <jchoo156@gmail.com>
2016-12-31 18:42:40 -0800

    attempt to practice more complex forms however got stuck with onChange with multiple inputs

--- ComplexForm.jsx
+++ ComplexForm.jsx
@@ -5,20 +5,30 @@ class ComplexForm extends React.Component {
     super(props);
 
     this.state = {
-      data: "INITIAL MOFUGGIN STATE"
+      data: "INITIAL MOFUGGIN STATE",
+      data2: "SECOND DATAAAAAAA"
     }
 
     this.updateState = this.updateState.bind(this);
   };
 
   updateState(e) {
-    this.setState({data: e.target.value});
+    this.setState({
+      data: e.target.value
+    });
+  }
+
+  updateState2(e) {
+    this.setState({
+      data: e.target.value
+    });
   }
 
   render() {
     return (
       <div>
         <Content initialData={this.state.data} updateData={this.updateState} />
+        <Content2 initialData={this.state.data2} updateData={this.updateState2} />
       </div>
     );
   }
@@ -30,6 +40,17 @@ class Content extends React.Component {
       <div>
         <input type="text" value={this.props.initialData} onChange={this.props.updateData} />
         <h3>{this.props.initialData}</h3>
+      </div>
+    );
+  }
+}
+
+class Content2 extends React.Component {
+  render() {
+    return (
+      <div>
+        <input type={this.props.data2} value={this.props.initialData} onChange={this.props.updateData} />
+        <h4>{this.props.initialData}</h4>
       </div>
     );
   }
